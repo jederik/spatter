283f18b2a0f5c30ddb79bbec6e453ee7315143d9
jederik <jederik@gmail.com>
2013-08-06 19:20:39 +0200

    Routing

--- spatter/src/SplatterRouting.java
+++ spatter/src/SplatterRouting.java
@@ -1,38 +1,146 @@
 import java.util.ArrayList;
 import java.util.List;
-
 	
 public class SplatterRouting {
 
-	public static final int NODE_COUNT = 10;
+	public static final int NODE_COUNT = 20;
 	public static final double ADVERTISING_ROUNDS = 5D;
+	public static final int ROUTING_ROUNDS = 100;
 
 	private static final int VECTOR_LENGTH = 10000;
 	
 	private static SCVector[] addresses, routes;
-	private static boolean[][] neighbors; 
+	private static boolean[][] neighbors;
 	
 	public static void main(String[] args) {
 		
+		// init network
 		initNodes();
 		initEdges(3);
 		System.out.println("Original Graph:");
 		displayGraph();
 		System.out.println();
-		
+
+		// advertise addresses
 		advertise(ADVERTISING_ROUNDS);
 		System.out.println("Learned Graph:");
 		displayRoutes();
+		System.out.println();
+		
+		// random routing
+		System.out.print("Running random routing tests...");
+		double avgHC = testRouting(randomRouter);
+		System.out.println("Average hop count (random routing):" + avgHC);
+		
+		// splatter routing
+		System.out.print("Running splatter routing tests...");
+		avgHC = testRouting(splatterRouter);
+		System.out.println("Average hop count (splatter routing):" + avgHC);
+		
 	}
 
-	public static final double THRESHOLD = .52D;
+	private static Router randomRouter = new Router() {
+		
+		@Override
+		public int getNextHop(int currentHop, int target) {
+			
+			// node isolated?
+			boolean isolated = true;
+			for (int j=0; j<NODE_COUNT; j++) {
+				if (neighbors[currentHop][j]) {
+					isolated = false;
+				}
+			}
+			if (isolated) {
+				return -1;
+			}
+
+			while (true) {
+				int j = (int) (Math.random() * NODE_COUNT);
+				if (neighbors[currentHop][j]) return j;
+			}
+		}
+	};
+	
+	private static Router splatterRouter = new Router() {
+		
+		@Override
+		public int getNextHop(int currentHop, int target) {
+			int nextHop = -1;
+			double bestMatch = 0D;
+			for (int j=0; j<NODE_COUNT; j++) {
+				if (neighbors[currentHop][j]) {
+					double match = SCVector.match(routes[j], addresses[target]);
+					if (match > bestMatch) {
+						nextHop = j;
+						bestMatch = match;
+					}
+				}
+			}
+			return nextHop;
+		}
+	};
+	
+	private static final int MAX_HOPS = NODE_COUNT * NODE_COUNT;
+
+	private static interface Router {
+		int getNextHop(int currentHop, int target);
+	}
+	
+	private static double testRouting(Router router) {
+
+		int fails = 0;
+		int totalHops = 0;
+		
+		for (int r=0; r<ROUTING_ROUNDS; r++) {
+			
+			int source = (int) (Math.random() * NODE_COUNT);
+			int target = (int) (Math.random() * NODE_COUNT);
+			
+			int hopCounter = 0;
+			
+			int i = source;
+			while (i != target && hopCounter <= MAX_HOPS) {
+				
+				// determine optimal next hop
+				int nextHop = router.getNextHop(i, target);
+				
+				// isolated node?
+				if (nextHop==-1) {
+					hopCounter = 0;
+					fails++;
+					break;
+				}
+				
+				hopCounter++;
+				i = nextHop;
+				
+			}
+			
+
+			if (hopCounter > MAX_HOPS) {
+				fails++;
+			}
+			
+			else {
+				totalHops += hopCounter;
+			}
+			
+		}
+		
+		System.out.println(fails + " fails");
+		return ((double) totalHops) / (ROUTING_ROUNDS-fails);
+		
+	}
+
+	public static final double THRESHOLD = .6D;
 	
 	private static void displayRoutes() {
 		for (int i=0; i<NODE_COUNT; i++) {
 			for (int j=0; j<NODE_COUNT; j++) {
-				boolean match = SCVector.match(routes[i], addresses[j]) > THRESHOLD;
-				System.out.print(match ? " 1" : " 0");
-				
+				double match = SCVector.match(routes[i], addresses[j]);
+//				System.out.print(match + "\t");
+				System.out.print(match > THRESHOLD ? " 1" : " 0");
 			}
 			System.out.println();
 		}
@@ -65,14 +173,14 @@ public class SplatterRouting {
 			List<SCVector> neighborRoutingVectors = new ArrayList<SCVector>();
 			for (int j=0; j<NODE_COUNT; j++) {
 				if (neighbors[node][j]) {
-//					SCVector route = SCVector.sum(routes[j], addresses[j]);
-					neighborRoutingVectors.add(routes[j]);
+/**/					SCVector route = SCVector.sum(routes[j], addresses[j]);
+					neighborRoutingVectors.add(route);
 				}
 			}
 			if (neighborRoutingVectors.size() == 0) continue;
 			SCVector neighborsum = SCVector.sum((SCVector[]) neighborRoutingVectors.toArray(dummy));
 			routes[node] = SCVector.sum(routes[node], neighborsum);
-/**/		routes[node] = SCVector.sum(routes[node], addresses[node]);
+//		routes[node] = SCVector.sum(routes[node], addresses[node]);
 		}
 	}
 
